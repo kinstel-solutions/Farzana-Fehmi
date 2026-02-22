a09a0458a3abe056479ebbc617314bc3e737b544
dethrtrns <rajshekharsingh060593@gmail.com>
2026-02-23 00:37:35 +0530

    caps fix

--- app/terms-of-service/page.tsx
+++ app/terms-of-service/page.tsx
@@ -25,7 +25,7 @@ export default function TermsOfServicePage() {
                 Overview
               </h2>
               <p>
-                Farzana Fehmi Designs offers this website, including all
+                Fehmi farzana designs offers this website, including all
                 information, tools, and services available from this site to
                 you, the user, conditioned upon your acceptance of all terms,
                 conditions, policies, and notices stated here.
